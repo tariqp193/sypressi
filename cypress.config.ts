5548a3e884753b1a9a066542d3c94d08a57749ad
Tarik Pelko <50297299+tariqp193@users.noreply.github.com>
2022-09-01 16:46:19 +0200

    config: Set run and media configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  viewportHeight: 1080, 
+  // General configuration
+  viewportHeight: 1080,
   viewportWidth: 1920,
   defaultCommandTimeout: 10000,
+  chromeWebSecurity: true,
+  pageLoadTimeout: 20000,
+
+  // Media configuration
+  video: false,
+  videosFolder: 'cypress/reports/videos',
+  screenshotsFolder: 'cypress/reports/screenshots',
+  screenshotOnRunFailure: true,
+  trashAssetsBeforeRuns: true,
+
+  // Test execution configuration 
+  retries: { "runMode": 2, "openMode": 0 },
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
