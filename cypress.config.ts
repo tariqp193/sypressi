307ea54e6931f71aa906a8c3eb0073377447aa1b
Tarik Pelko <50297299+tariqp193@users.noreply.github.com>
2022-09-01 16:39:30 +0200

    config: Set viewport of test execution

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportHeight: 1080, 
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
